1bbac0c94c38cb55eff4cfd79df09d6d7b0fec1f
Volodymyr Mironov <kuomolew@gmail.com>
2023-06-18 15:33:28 +0300

    add test src code

---
+++ src/shared/js/main.js
@@ -0,0 +1,3 @@
+console.log('slides', window.navi.slides());
+console.log('all slides', window.navi.allSlides());
+console.log('hidden slides', window.navi.hiddenSlides());
---
+++ src/slides/slide5/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable prefer-arrow-callback */
+// eslint-disable-next-line no-undef, func-names
+$(window).on('load', function () {
+  console.log('window loaded');
+});
